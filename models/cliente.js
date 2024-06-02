66773b47adac69fe1492ed55302154a2fc9926c3
Juan Sebastian Rojas Sanchez <sebastian1116234435@gmail.com>
2024-06-01 23:03:18 -0500

    iteracion 2

--- models/cliente.js
+++ models/cliente.js
@@ -1,85 +1,28 @@
-const Cliente = require('../models/cliente');
-const { request, response } = require('express');
+const { Schema, model } = require('mongoose');
 
-const createCliente = async (req = request, res = response) => {
-    const { nombre, telefono, direccion } = req.body;
-    try {
-        const cliente = new Cliente({
-            nombre,
-            telefono,
-            direccion
-        });
-        await cliente.save();
-        return res.status(200).json(cliente);
-    } catch (error) {
-        console.error('Error al crear el cliente:', error);
-        return res.status(500).json({ mensaje: 'Error interno del servidor' });
+const ClienteSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    telefono: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    direccion: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now
+    },
+    fechaActualizacion: {
+        type: Date,
+        default: new Date()
     }
-}
+});
 
-const getCliente = async (req = request, res = response) => {
-    try {
-        const clientes = await Cliente.find();
-        return res.status(200).json(clientes);
-    } catch (error) {
-        console.error('Error al obtener los clientes:', error);
-        return res.status(500).json({ mensaje: 'Error interno del servidor' });
-    }
-};
-
-const getClienteById = async (req = request, res = response) => {
-    const { id } = req.params;
-    try {
-        const cliente = await Cliente.findById(id);
-        if (!cliente) {
-            return res.status(404).json({ mensaje: 'Cliente no encontrado' });
-        }
-        return res.status(200).json(cliente);
-    } catch (error) {
-        console.error('Error al obtener el cliente:', error);
-        return res.status(500).json({ mensaje: 'Error interno del servidor' });
-    }
-};
-
-const updateCliente = async (req = request, res = response) => {
-    const { id } = req.params;
-    const updateData = req.body;
-
-    try {
-        const cliente = await Cliente.findByIdAndUpdate(id, updateData, { new: true });
-
-        if (!cliente) {
-            return res.status(404).json({ mensaje: 'Cliente no encontrado' });
-        }
-
-        return res.status(200).json(cliente);
-    } catch (error) {
-        console.error('Error al actualizar el cliente:', error);
-        return res.status(500).json({ mensaje: 'Error interno del servidor' });
-    }
-};
-
-const deleteCliente = async (req = request, res = response) => {
-    const { id } = req.params;
-
-    try {
-        const cliente = await Cliente.findByIdAndDelete(id);
-
-        if (!cliente) {
-            return res.status(404).json({ mensaje: 'Cliente no encontrado' });
-        }
-
-        return res.status(204).json();
-    } catch (error) {
-        console.error('Error al eliminar el cliente:', error);
-        return res.status(500).json({ mensaje: 'Error interno del servidor' });
-    }
-};
-
-module.exports = {
-    createCliente,
-    getCliente,
-    getClienteById,
-    updateCliente,
-    deleteCliente,
-};
+module.exports = model('Cliente', ClienteSchema);
--- routes/vehiculo.js
+++ routes/vehiculo.js
@@ -1,10 +1,18 @@
 const { Router } = require('express');
 const {
     createVehiculo,
+    getVehiculo,
+    getVehiculoById,
+    updateVehiculo,
+    deleteVehiculo,
 } = require('../controllers/vehiculo');
 
 const router = Router();
 
 router.post('/', createVehiculo);
+router.get('/', getVehiculo);
+router.get('/:id', getVehiculoById);
+router.put('/:id', updateVehiculo);
+router.delete('/:id', deleteVehiculo);
 
 module.exports = router;
